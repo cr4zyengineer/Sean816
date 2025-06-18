1c8acbe0967d512f37470829da74afcc9341a55e
seanistethered <fridakitten@pm.me>
2025-06-18 09:16:19 +0200

    Memory improvements

--- memory.h
+++ memory.h
@@ -13,9 +13,6 @@
 #define MEMORY_MAPPED_IO_REGION_SIZE 0x00FF
 
 typedef struct {
-    // RAW Memory Stuff
-    uint8_t value;
-
     // IO Stuff
     bool isSet;
     void (*rfunction)(uint16_t addr, uint8_t *value);
@@ -34,5 +31,3 @@ void memory_io_set(uint16_t addr, void *rfunction, void *wfunction);
  */
 void memory_read(uint16_t addr, uint8_t *value);
 void memory_write(uint16_t addr, uint8_t value);
-void memory_buffer_intl_exec(uint8_t buffer[UINT16_MAX - MEMORY_MAPPED_IO_REGION_SIZE]);
-void memory_load_binary(const char *filePath);
